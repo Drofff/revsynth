5471f8a99ea42af03650e00dd7c59d6e3a346aa6
Mykhailo Palahuta <mykhailo.palahuta@care.com>
2023-04-07 01:58:28 +0300

    random select

--- aco/slice.go
+++ aco/slice.go
@@ -1,5 +1,10 @@
 package aco
 
+import (
+	"log"
+	"math/rand"
+)
+
 func sumFloat64(nums []float64) float64 {
 	sum := 0.0
 	for _, num := range nums {
@@ -8,6 +13,24 @@ func sumFloat64(nums []float64) float64 {
 	return sum
 }
 
+func probToInt(prob float64) int {
+	return int(prob * 100000.0)
+}
+
 func chooseRand(probs []float64) int {
+	probsSum := 0
+	for _, prob := range probs {
+		probsSum += probToInt(prob)
+	}
+
+	selector := rand.Intn(probsSum)
+	for i, prob := range probs {
+		selector -= probToInt(prob)
+		if selector < 0 {
+			return i
+		}
+	}
+
+	log.Fatalln("unexpected random select result for:", probs)
 	return -1
 }
