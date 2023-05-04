05033096ab9162d7b9d744dd2ad7b77adec6a457
Mykhailo Palahuta <mykhailo.palahuta@care.com>
2023-05-05 00:07:57 +0300

    use bin to dec in tt <-> tv

--- circuit/truthtable.go
+++ circuit/truthtable.go
@@ -1,6 +1,9 @@
 package circuit
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 type TruthTable struct {
 	Rows []TruthTableRow
@@ -32,24 +35,26 @@ func (tt TruthTable) Copy() TruthTable {
 	return TruthTable{Rows: cRows}
 }
 
+func binToDec(bins []int) int {
+	binStr := ""
+	for _, bin := range bins {
+		binStr += strconv.Itoa(bin)
+	}
+	dec, err := strconv.ParseInt(binStr, 2, 32)
+	if err != nil {
+		panic(err)
+	}
+	return int(dec)
+}
+
 func (tt TruthTable) ToVector() TruthVector {
 
 	ins := make([][]int, 0)
 	v := make([]int, 0)
 
-	for _, outRow := range tt.Rows {
-
-		ins = append(ins, outRow.Input)
-
-		for inRowIndex, inRow := range tt.Rows {
-
-			if equal(outRow.Output, inRow.Input) {
-				v = append(v, inRowIndex)
-				break
-			}
-
-		}
-
+	for _, row := range tt.Rows {
+		ins = append(ins, row.Input)
+		v = append(v, binToDec(row.Output))
 	}
 
 	return TruthVector{Inputs: ins, Vector: v}
@@ -71,13 +76,30 @@ func (ttr TruthTableRow) Copy() TruthTableRow {
 	return TruthTableRow{Input: cin, Output: cout}
 }
 
+func decToBin(dec int, binLen int) []int {
+	bin := make([]int, binLen)
+
+	binStr := strconv.FormatInt(int64(dec), 2)
+	binI := binLen - 1
+	for strI := len(binStr) - 1; strI >= 0; strI-- {
+		if binStr[strI] == '1' {
+			bin[binI] = 1
+		}
+
+		binI--
+	}
+
+	return bin
+}
+
 func (tv TruthVector) ToTable() TruthTable {
 
 	tt := TruthTable{Rows: make([]TruthTableRow, 0)}
+	outputLen := len(tv.Inputs[0])
 
 	for i := range tv.Vector {
 		input := tv.Inputs[i]
-		output := tv.Inputs[tv.Vector[i]]
+		output := decToBin(tv.Vector[i], outputLen)
 
 		row := TruthTableRow{
 			Input:  input,
