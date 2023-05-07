f32c461e8803414b5832e4ede689896c3b187964
Mykhailo Palahuta <mykhailo.palahuta@care.com>
2023-05-07 13:03:08 +0300

    main.go: half adder input

--- main.go
+++ main.go
@@ -12,31 +12,26 @@ import (
 
 func main() {
 	conf := aco.Config{
-		NumOfAnts:       20,
+		NumOfAnts:       100,
 		NumOfIterations: 30,
 		Alpha:           2.0,
 		Beta:            1.5,
-		EvaporationRate: 0.3,
+		EvaporationRate: 0.4,
 		DepositStrength: 100,
 
-		LocalLoops:  4,
-		SearchDepth: 6,
+		LocalLoops:  20,
+		SearchDepth: 10,
 	}
-	synth := aco.NewSynthesizer(conf, []circuit.GateFactory{
-		circuit.NewFredkinGateFactory(), circuit.NewNotGateFactory()}, logging.NewLogger(logging.LevelInfo))
+	synth := aco.NewSynthesizer(conf, []circuit.GateFactory{circuit.NewToffoliGateFactory()}, logging.NewLogger(logging.LevelInfo))
 
 	fmt.Println("Running synthesis..")
 	startedAt := time.Now().UnixMilli()
 	desiredVector := circuit.TruthVector{Inputs: [][]int{
 		{0, 0, 0, 1},
-		{0, 0, 1, 1},
 		{0, 1, 0, 1},
-		{0, 1, 1, 1},
 		{1, 0, 0, 1},
-		{1, 0, 1, 1},
 		{1, 1, 0, 1},
-		{1, 1, 1, 1},
-	}, Vector: []int{1, 0, 3, 2, 5, 7, 4, 6}, AdditionalLinesMask: []int{1, 1, 1, 0}}
+	}, Vector: []int{0, 2, 2, 1}, AdditionalLinesMask: []int{1, 1, 0, 0}}
 	res := synth.Synthesise(desiredVector)
 
 	processingTime := time.Now().UnixMilli() - startedAt
