3b8cfa0d325dd1a56488186f0b19b7dd014ebee0
Mykhailo Palahuta <mykhailo.palahuta@care.com>
2024-05-01 21:44:57 +0300

    continuous search

--- main.go
+++ main.go
@@ -14,38 +14,52 @@ const pheromonesOutputFile = "pheromones.gv"
 
 func main() {
 	conf := aco.Config{
-		NumOfAnts:       35,
-		NumOfIterations: 20,
-		Alpha:           3.0,
+		NumOfAnts:       30,
+		NumOfIterations: 15,
+		Alpha:           1,
 		Beta:            -1,
 		EvaporationRate: 0.4,
 		DepositStrength: 100,
 
-		LocalLoops:  7,
-		SearchDepth: 8,
+		LocalLoops:  15,
+		SearchDepth: 4,
+
+		AllowedControlBitValues: []int{circuit.ControlBitPositive, circuit.ControlBitIgnore},
 	}
 	synth := aco.NewSynthesizer(conf,
 		[]circuit.GateFactory{circuit.NewToffoliGateFactory()},
 		logging.NewLogger(logging.LevelInfo))
 
-	fmt.Println("Running synthesis..")
-	startedAt := time.Now().UnixMilli()
 	desiredVector := circuit.TruthVector{Inputs: [][]int{
-		{0, 0, 0},
-		{0, 0, 1},
-		{0, 1, 0},
-		{0, 1, 1},
-		{1, 0, 0},
-		{1, 0, 1},
-		{1, 1, 0},
-		{1, 1, 1},
-	}, Vector: []int{2, 6, 0, 5, 7, 3, 4, 1}, AdditionalLinesMask: []int{}}
-	res := synth.Synthesise(desiredVector)
+		{0, 0, 0, 0},
+		{0, 0, 0, 1},
+		{0, 0, 1, 0},
+		{0, 0, 1, 1},
+		{0, 1, 0, 0},
+		{0, 1, 0, 1},
+		{0, 1, 1, 0},
+		{0, 1, 1, 1},
+	}, Vector: []int{0, 2, 6, 1, 14, 5, 13, 3}, AdditionalLinesMask: []int{}}
 
-	processingTime := time.Now().UnixMilli() - startedAt
+	var res aco.SynthesisResult
+	synthIteration := 0
+	for {
+		fmt.Printf("Running synthesis.. %v iteration\n", synthIteration)
+		synthIteration++
 
-	fmt.Println("==========================")
-	fmt.Printf("Processing time: %v millis\n", processingTime)
+		startedAt := time.Now().UnixMilli()
+
+		res = synth.Synthesise(desiredVector)
+
+		processingTime := time.Now().UnixMilli() - startedAt
+		fmt.Println("==========================")
+		fmt.Printf("Processing time: %v millis\n", processingTime)
+
+		if res.Complexity == 0 {
+			break
+		}
+		fmt.Printf("continue synthesizing, got complexity %v\n", res.Complexity)
+	}
 
 	fmt.Printf("Saving pheromoes graph in %v\n", pheromonesOutputFile)
 	err := cli.SaveGraph(res.Pheromones, pheromonesOutputFile)
